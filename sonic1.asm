7ef020607dd7013c2270e76a9bcd5c83c2485a68
Dude <fallenone0009@yahoo.co.jp>
2018-12-10 03:00:25 -0500

    Update 3

--- sonic1.asm
+++ sonic1.asm
@@ -10276,14 +10276,6 @@ LoadTilesAsYouMove:
 	lea	($FFFFFF36).w,a2
 	lea	($FFFFFF28).w,a3
 	bsr.w	sub_DD82
-;	jmp	loc_DAAE
-;	lea	(Scroll_flags_copy_P2).w,a2
-;	lea	($FFFFEE80).w,a3
-;	movea.l	($FFFFA400).w,a4
-;	move.w	#$6000,d2
-;	bsr.w	sub_DB5C
-
-;loc_DAAE:
 	lea	($FFFFFF30).w,a2
 	lea	($FFFFFF10).w,a3
 	movea.l	($FFFFA400).w,a4
@@ -10789,8 +10781,6 @@ word_DE7E:
 
 loc_DE86:
 
-;	tst.w	(Two_player_mode).w
-;	bne.s	loc_DEC8
 	moveq	#$F,d6
 	move.l	#$800000,d7
 
@@ -10815,32 +10805,6 @@ loc_DEBC:
 	rts
 ; ===========================================================================
 
-;loc_DEC8:
-;	moveq	#$F,d6
-;	move.l	#$800000,d7
-
-;loc_DED0:
-;	moveq	#0,d0
-;	move.b	(a0)+,d0
-;	btst	d0,(a2)
-;	beq.s	loc_DEF8
-;	movea.w	word_DE7E(pc,d0.w),a3
-;	movem.l	d4-d5/a0,-(sp)
-;	movem.l	d4-d5,-(sp)
-;	bsr.w	sub_E244
-;	movem.l	(sp)+,d4-d5
-;	bsr.w	Calc_VRAM_Pos
-;	bsr.w	sub_E1FA
-;	movem.l	(sp)+,d4-d5/a0
-
-;loc_DEF8:
-;	addi.w	#$10,d4
-;	dbf	d6,loc_DED0
-;	clr.b	(a2)
-;	rts
-; End of function sub_DD82
-
-
 ; ||||||||||||||| S U B R O U T I N E |||||||||||||||||||||||||||||||||||||||
 
 
@@ -10854,8 +10818,6 @@ loc_DF06:
 	move.l	#$800000,d7
 	move.l	d0,d1
 	bsr.w	sub_E09E
-;	tst.w	(Two_player_mode).w
-;	bne.s	loc_DF54
 
 loc_DF1E:
 	move.w	(a0),d3
@@ -10900,8 +10862,6 @@ DrawTiles_Vertical2:
 	add.w	4(a3),d4
 ; loc_DF9A:
 DrawTiles_Vertical3:
-;	tst.w	(Two_player_mode).w
-;	bne.s	loc_E018
 	move.l	a2,-(sp)
 	move.w	d6,-(sp)
 	lea	($FFFFF380).w,a2
@@ -10958,74 +10918,6 @@ loc_E010:
 	movea.l	(sp)+,a2
 	rts
 ; ===========================================================================
-
-;loc_E018:
-;	move.l	d0,d1
-;	or.w	d2,d1
-;	swap	d1
-;	move.l	d1,(a5)
-;	swap	d1
-;	tst.b	d1
-;	bmi.s	loc_E062
-;	bsr.w	sub_E09E
-
-;loc_E02A:
-;	move.w	(a0),d3
-;	andi.w	#$3FF,d3
-;	lsl.w	#3,d3
-;	lea	($FFFFB000).w,a1
-;	adda.w	d3,a1
-;	bsr.w	sub_E136
-;	addq.w	#2,a0
-;	addq.b	#4,d1
-;	bpl.s	loc_E04C
-;	andi.b	#$7F,d1
-;	swap	d1
-;	move.l	d1,(a5)
-;	swap	d1
-
-;loc_E04C:
-;	addi.w	#$10,d5
-;	move.w	d5,d0
-;	andi.w	#$70,d0
-;	bne.s	loc_E05C
-;	bsr.w	sub_E09E
-
-;loc_E05C:
-;	dbf	d6,loc_E02A
-;	rts
-; ===========================================================================
-
-;loc_E062:
-;	bsr.w	sub_E09E
-
-;loc_E066:
-;	move.w	(a0),d3
-;	andi.w	#$3FF,d3
-;	lsl.w	#3,d3
-;	lea	($FFFFB000).w,a1
-;	adda.w	d3,a1
-;	bsr.w	sub_E136
-;	addq.w	#2,a0
-;	addq.b	#4,d1
-;	bmi.s	loc_E088
-;	ori.b	#$80,d1
-;	swap	d1
-;	move.l	d1,(a5)
-;	swap	d1
-
-;loc_E088:
-;	addi.w	#$10,d5
-;	move.w	d5,d0
-;	andi.w	#$70,d0
-;	bne.s	loc_E098
-;	bsr.w	sub_E09E
-
-;loc_E098:
-;	dbf	d6,loc_E066
-;	rts
-; End of function DrawTiles_Vertical1
-
 
 ; ||||||||||||||| S U B R O U T I N E |||||||||||||||||||||||||||||||||||||||
 
@@ -11214,51 +11106,6 @@ loc_E1D6:
 ; ||||||||||||||| S U B R O U T I N E |||||||||||||||||||||||||||||||||||||||
 
 
-;sub_E1FA:
-
-;	or.w	d2,d0
-;	swap	d0
-;	btst	#3,(a0)
-;	bne.s	loc_E220
-;	btst	#2,(a0)
-;	bne.s	loc_E210
-;	move.l	d0,(a5)
-;	move.l	(a1)+,(a6)
-;	rts
-; ===========================================================================
-
-;loc_E210:
-;	move.l	d0,(a5)
-;	move.l	(a1)+,d3
-;	eori.l	#$8000800,d3
-;	swap	d3
-;	move.l	d3,(a6)
-;	rts
-; ===========================================================================
-
-;loc_E220:
-;	btst	#2,(a0)
-;	bne.s	loc_E234
-;	move.l	d0,(a5)
-;	move.l	(a1)+,d3
-;	eori.l	#$10001000,d3
-;	move.l	d3,(a6)
-;	rts
-; ===========================================================================
-
-;loc_E234:
-;	move.l	d0,(a5)
-;	move.l	(a1)+,d3
-;	eori.l	#$18001800,d3
-;	swap	d3
-;	move.l	d3,(a6)
-;	rts
-; End of function sub_E1FA
-
-
-; ||||||||||||||| S U B R O U T I N E |||||||||||||||||||||||||||||||||||||||
-
-
 sub_E244:
 
 	add.w	(a3),d5
@@ -11297,8 +11144,6 @@ Calc_VRAM_Pos:
 	add.w	(a3),d5
 
 loc_E288:
-;	tst.w	(Two_player_mode).w
-;	bne.s	loc_E2A8
 	add.w	4(a3),d4
 	andi.w	#$F0,d4
 	andi.w	#$1F0,d5
@@ -11310,9 +11155,6 @@ loc_E288:
 	move.w	d4,d0
 	rts
 ; ===========================================================================
-
-;loc_E2A8:
-;	add.w	4(a3),d4
 
 loc_E2AC:
 	andi.w	#$1F0,d4
@@ -11331,8 +11173,6 @@ loc_E2AC:
 
 
 loc_E2C2:
-;	tst.w	(Two_player_mode).w
-;	bne.s	loc_E2E4
 	add.w	4(a3),d4
 	add.w	(a3),d5
 	andi.w	#$F0,d4
@@ -11345,20 +11185,6 @@ loc_E2C2:
 	move.w	d4,d0
 	rts
 ; ===========================================================================
-; interestingly, this subroutine was in the sonic 1 rom, unused
-;loc_E2E4:
-;	add.w	4(a3),d4
-;	add.w	(a3),d5
-;	andi.w	#$1F0,d4
-;	andi.w	#$1F0,d5
-;	lsl.w	#3,d4
-;	lsr.w	#2,d5
-;	add.w	d5,d4
-;	moveq	#2,d0
-;	swap	d0
-;	move.w	d4,d0
-;	rts
-; End of function k
 
 ; ===========================================================================
 
